c4c76e6948b1d5e0c049157d402a6661b244f79e
KimTaeyeon-08 <ssg9732@gmail.com>
2022-01-12 02:34:11 +0900

    upload script

---
+++ GoToUrl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GoToUrl : MonoBehaviour
+{
+    public Text Word;
+    public void OpenURL()
+    {
+        Application.OpenURL($"https://www.google.com/search?q={Word.text}");
+        /*Application.OpenURL($"https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={Word.text}");*/
+    }
+}
